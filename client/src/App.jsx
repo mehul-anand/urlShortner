755c2cca00ca0fea2fc1dc7379ca38f1e8d2535b
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-02-25 01:50:28 +0530

    testing toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner"
+import { toast } from "sonner"
 function App() {
   return (
     <>
-      <h1 className="text-7xl">Hello</h1>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-7xl">Hello</h1>
+        <Toaster />
+        <Button
+          onClick={() => {
+            toast("YAY")
+            ;
+          }}
+        >
+          Click me
+        </Button>
+      </div>
     </>
   );
 }
