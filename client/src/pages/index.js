0399dcd5c7b187166aa25c6b3548ded80955ea4f
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-03-27 02:15:04 +0530

    page exports

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Auth from "./Auth";
+import Dashboard from "./Dashboard";
+import Landing from "./Landing";
+import Layout from "./Layout";
+import Link from "./Link";
+import Redirect from "./Redirect";
+
+export default { Auth, Dashboard, Landing, Layout, Redirect , Link };
