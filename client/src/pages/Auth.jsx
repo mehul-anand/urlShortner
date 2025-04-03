a5d3eaca97694698808e677cb5b69c4046543763
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-04-03 05:46:32 +0530

    auth setup

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useSearchParams } from "react-router";
+import Login from "@/components/Login";
+import Signup from "@/components/Signup";
 
 const Auth = () => {
+  const [searchedParams] = useSearchParams();
   return (
-    <div>Auth</div>
-  )
-}
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchedParams.get("shorten")
+          ? "Let's Login first.."
+          : "Login / Sign Up"}
+      </h1>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="signup">
+          <Signup/>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
 
-export default Auth
+export default Auth;
