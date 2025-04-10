f3350eb4d155ea6bbab7361539e3a67e61c547d0
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-04-10 06:17:38 +0530

    imporving the auth component

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import Login from "@/components/Login";
 import Signup from "@/components/Signup";
+import { UrlState } from "@/context";
 
 const Auth = () => {
   const [searchedParams] = useSearchParams();
+  const {isAuthenticated,loading} = UrlState()
+  const navigate = useNavigate()
+  const userLink = searchedParams.get("shorten");
+  useEffect(()=>{
+    console.log(`isAuth:${isAuthenticated}`)
+    if(isAuthenticated && !loading){
+      navigate(`/dashboard?${userLink ? `shorten=${userLink}` : ""}`)
+    }
+  },[isAuthenticated,loading])
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
-      <h1 className="text-5xl font-extrabold">
-        {searchedParams.get("shorten")
+      <h1 className="sm:text-5xl text-3xl font-extrabold">
+        {userLink
           ? "Let's Login first.."
           : "Login / Sign Up"}
       </h1>
-      <Tabs defaultValue="account" className="w-[400px]">
+      <Tabs defaultValue="login" className="sm:w-[400px] px-3 w-11/12">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="signup">Sign Up</TabsTrigger>
