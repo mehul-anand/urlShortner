0023e76eeb5e4617cfdbf95c970e139ef6e45929
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-04-10 06:16:58 +0530

    url api

---
+++ client/src/db/apiUrls.js
@@ -0,0 +1,13 @@
+import supabase, { supabaseUrl } from "./supabase";
+export async function getUrls(user_id) {
+  //getting all URLs for that user
+  let { data, error } = await supabase
+    .from("urls")
+    .select("*")
+    .eq("user_id", user_id);
+  if (error) {
+    console.error(error.message);
+    throw new Error("Unable to load URLs");
+  }
+  return data;
+}
