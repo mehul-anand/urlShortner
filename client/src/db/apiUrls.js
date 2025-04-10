c3b9742b432898e2cdf6a57cfe87c71c9eb2d5cc
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-04-11 01:00:07 +0530

    delete url : api supabase

--- client/src/db/apiUrls.js
+++ client/src/db/apiUrls.js
@@ -11,3 +11,12 @@ export async function getUrls(user_id) {
   }
   return data;
 }
+export async function deleteUrl(url_id) {
+  //to delete a particular URL
+  let { data, error } = await supabase.from("urls").delete("").eq("id", url_id);
+  if (error) {
+    console.error(error.message);
+    throw new Error("Unable to delete the URL");
+  }
+  return data;
+}
