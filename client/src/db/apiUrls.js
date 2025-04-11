d2464e34c43d3529ae48a2e21d1ede4895406acd
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-04-11 05:34:19 +0530

    created at function : api supabase

--- client/src/db/apiUrls.js
+++ client/src/db/apiUrls.js
@@ -20,3 +20,39 @@ export async function deleteUrl(url_id) {
   }
   return data;
 }
+
+export async function createUrl(
+  { title, longUrl, customUrl, user_id },
+  qrCode
+) {
+  // generating a short url
+  const shortUrl = Math.random().toString(36).substring(2, 6);
+  // uploading QR
+  const fileName = `qr-${shortUrl}`;
+  const { error: storageError } = await supabase.storage
+    .from("qr")
+    .upload(fileName, qrCode);
+  if (storageError) {
+    throw new Error(storageError.error);
+  }
+  //url creation
+  const supa_qr = `${supabaseUrl}/storage/v1/object/public/qr/${fileName}`;
+  let { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        destination_url: longUrl,
+        custom_url: customUrl || null,
+        user_id,
+        short_url: shortUrl,
+        qr: supa_qr,
+      },
+    ])
+    .select();
+  if (error) {
+    console.error(error.message);
+    throw new Error("Unable to create the short URL");
+  }
+  return data;
+}
